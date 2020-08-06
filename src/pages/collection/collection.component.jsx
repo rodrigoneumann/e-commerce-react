0a9da4cd421135d703b290c754cdd2fbae214c59
Rodrigo Neumann de Souza <rodrigoneumann@gmail.com>
2020-08-06 13:26:58 +0100

    implemented react-thunks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,13 +3,13 @@ import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
 import {
   CollectionPageContainer,
   CollectionTitle,
   CollectionItemsContainer,
 } from "./collection.styles";
-
-import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
